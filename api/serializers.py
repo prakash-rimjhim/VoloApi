aff6aaa77524fcd024c1dcb4b7fdb7f43501d648
Rimjhim Prakash <rimjhimprakash1998@gmail.com>
2023-06-18 17:16:42 +0530

    delete

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import  Data
-
-class DataSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Data
-        fields = '__all__'
